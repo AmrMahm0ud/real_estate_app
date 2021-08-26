73f1199f84e41eb8627bb2e99e92309e5fb7cd86
Amr <amrmahmoud710gmail.com>
2021-08-26 08:32:55 +0200

    enhance code base

--- lib/ui/widget/item_card_widget.dart
+++ lib/ui/widget/item_card_widget.dart
@@ -30,142 +30,111 @@ class _ItemCardWidgetState extends State<ItemCardWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 4.0, right: 4.0),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12.0),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.6),
-              spreadRadius: 2,
-              blurRadius: 4,
-              offset: Offset(0, 7), // changes position of shadow
-            ),
-          ],
-        ),
-        height: 120,
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(12.0)),
+      ),
+      elevation: 10.0,
+      child: Row(
+        children: [
+          Container(
+            width: 140,
+            child: sliderImageCardWidget(widget.sliderCardImages),
           ),
-          elevation: 0.0,
-          child: Row(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      bottomLeft: Radius.circular(12)),
-                ),
-                width: 140,
-                child: sliderImageCardWidget(widget.sliderCardImages),
-              ),
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12.0),
-                      color: Colors.white),
-                  child: Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+          Expanded(
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Row(
                       children: [
-                        Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  widget.category.toUpperCase(),
-                                  style: TextStyle(color: Colors.grey),
-                                ),
-                              ),
-                              Icon(
-                                Icons.favorite_border,
-                                color: Colors.grey,
-                              )
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 4.0),
+                        Expanded(
                           child: Text(
-                            widget.price + " AED",
-                            style: TextStyle(fontSize: 13.0),
+                            widget.category.toUpperCase(),
+                            style: TextStyle(color: Colors.grey),
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12.0),
-                            ),
-                            width: 100,
-                            child: Text(
-                              widget.address,
-                              maxLines: 2,
-                              style: TextStyle(
-                                  fontSize: 13.0, color: Colors.black),
-                            ),
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 4.0),
-                              child: Icon(
-                                Icons.king_bed_outlined,
-                                size: 22,
-                                color: Colors.black54,
-                              ),
-                            ),
-                            Text(
-                              "${removeDecimalZeroFormat(widget.bedRooms)}",
-                              style: TextStyle(
-                                fontSize: 11.0,
-                                color: Colors.black54,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 6.0),
-                              child: Icon(
-                                Icons.bathtub_outlined,
-                                size: 22,
-                                color: Colors.black54,
-                              ),
-                            ),
-                            Text(
-                              "${removeDecimalZeroFormat(widget.bathRooms)}",
-                              style: TextStyle(
-                                fontSize: 11.0,
-                                color: Colors.black54,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 6.0),
-                              child: Icon(
-                                Icons.apartment,
-                                size: 22,
-                                color: Colors.black54,
-                              ),
-                            ),
-                            Text(
-                              "${widget.areaSpace}" + " sqft",
-                              style: TextStyle(
-                                fontSize: 11.0,
-                                color: Colors.black54,
-                              ),
-                            )
-                          ],
+                        Icon(
+                          Icons.favorite_border,
+                          color: Colors.grey,
                         )
                       ],
                     ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4.0),
+                    child: Text(
+                      widget.price + " AED",
+                      style: TextStyle(fontSize: 13.0),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Container(
+                      width: 100,
+                      child: Text(
+                        widget.address,
+                        maxLines: 2,
+                        style: TextStyle(fontSize: 13.0, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4.0),
+                        child: Icon(
+                          Icons.king_bed_outlined,
+                          size: 22,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      Text(
+                        "${removeDecimalZeroFormat(widget.bedRooms)}",
+                        style: TextStyle(
+                          fontSize: 11.0,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 6.0),
+                        child: Icon(
+                          Icons.bathtub_outlined,
+                          size: 22,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      Text(
+                        "${removeDecimalZeroFormat(widget.bathRooms)}",
+                        style: TextStyle(
+                          fontSize: 11.0,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 6.0),
+                        child: Icon(
+                          Icons.apartment,
+                          size: 22,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      Text(
+                        "${widget.areaSpace}" + " sqft",
+                        style: TextStyle(
+                          fontSize: 11.0,
+                          color: Colors.black54,
+                        ),
+                      )
+                    ],
+                  )
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
@@ -180,20 +149,22 @@ class _ItemCardWidgetState extends State<ItemCardWidget> {
       children: [
         CarouselSlider(
           items: imagesUrl
-              .map((image) => Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(12.0),
-                        topLeft: Radius.circular(12.0),
-                      ),
-                      image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image: NetworkImage(
-                          image,
-                        ),
+              .map(
+                (image) => Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(12.0),
+                      topLeft: Radius.circular(12.0),
+                    ),
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(
+                        image,
                       ),
                     ),
-                  ))
+                  ),
+                ),
+              )
               .toList(),
           options: CarouselOptions(
             onPageChanged: (index, reason) {
--- lib/ui/widget/silder_images.dart
+++ lib/ui/widget/silder_images.dart
@@ -27,114 +27,100 @@ class SliderImagesWidget extends StatelessWidget {
 //////////helper Widget///////////
 /////////////////////////////////
   Widget imageWidget(BuildContext context, SliderModel sliderModel) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 2,
-            blurRadius: 4,
-            offset: Offset(0, 7), // changes position of shadow
-          ),
-        ],
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
       ),
-      width: MediaQuery.of(context).size.width,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        elevation: 0.0,
-        margin: EdgeInsets.symmetric(horizontal: 3.0),
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: NetworkImage(sliderModel.sliderImage))),
-            ),
-            Positioned(
-                left: 0.0,
-                bottom: 0.0,
-                right: 0.0,
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(12),
-                          bottomRight: Radius.circular(12)),
-                      gradient: LinearGradient(
-                          colors: [Colors.white, Colors.transparent])),
-                  height: 60.0,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        top: 5.0, left: 10.0, right: 10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Text(
-                                sliderModel.category.toUpperCase(),
-                                style: TextStyle(color: Colors.grey),
-                              ),
+      elevation: 10.0,
+      margin: EdgeInsets.symmetric(horizontal: 2.0, vertical: 4.0),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: NetworkImage(sliderModel.sliderImage))),
+          ),
+          Positioned(
+              left: 0.0,
+              bottom: 0.0,
+              right: 0.0,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(12),
+                        bottomRight: Radius.circular(12)),
+                    gradient: LinearGradient(
+                        colors: [Colors.white, Colors.transparent])),
+                height: 60.0,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(top: 5.0, left: 10.0, right: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              sliderModel.category.toUpperCase(),
+                              style: TextStyle(color: Colors.grey),
                             ),
-                            Text(
-                              sliderModel.price + " AED",
-                              style: TextStyle(
-                                  fontSize: 13.0, color: Colors.black),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          sliderModel.address,
-                          style: TextStyle(fontSize: 13, color: Colors.black45),
-                        ),
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 4.0),
-                              child: Icon(Icons.king_bed_outlined,
-                                  size: 22, color: Colors.black45),
-                            ),
-                            Text(
-                              "${removeDecimalZeroFormat(sliderModel.bedRooms)}",
+                          ),
+                          Text(
+                            sliderModel.price + " AED",
+                            style:
+                                TextStyle(fontSize: 13.0, color: Colors.black),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        sliderModel.address,
+                        style: TextStyle(fontSize: 13, color: Colors.black45),
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 4.0),
+                            child: Icon(Icons.king_bed_outlined,
+                                size: 22, color: Colors.black45),
+                          ),
+                          Text(
+                            "${removeDecimalZeroFormat(sliderModel.bedRooms)}",
+                            style: TextStyle(
+                                fontSize: 11.0, color: Colors.black45),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 6.0),
+                            child: Icon(Icons.bathtub_outlined,
+                                size: 22, color: Colors.black45),
+                          ),
+                          Text(
+                            "${removeDecimalZeroFormat(sliderModel.bathRooms)}",
+                            style: TextStyle(
+                                fontSize: 11.0, color: Colors.black45),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 6.0),
+                            child: Icon(Icons.apartment,
+                                size: 22, color: Colors.black45),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 6.0),
+                            child: Text(
+                              "${sliderModel.areaSpace}" + " sqft",
                               style: TextStyle(
                                   fontSize: 11.0, color: Colors.black45),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 6.0),
-                              child: Icon(Icons.bathtub_outlined,
-                                  size: 22, color: Colors.black45),
-                            ),
-                            Text(
-                              "${removeDecimalZeroFormat(sliderModel.bathRooms)}",
-                              style: TextStyle(
-                                  fontSize: 11.0, color: Colors.black45),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 6.0),
-                              child: Icon(Icons.apartment,
-                                  size: 22, color: Colors.black45),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 6.0),
-                              child: Text(
-                                "${sliderModel.areaSpace}" + " sqft",
-                                style: TextStyle(
-                                    fontSize: 11.0, color: Colors.black45),
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
+                          )
+                        ],
+                      )
+                    ],
                   ),
-                )),
-          ],
-        ),
+                ),
+              )),
+        ],
       ),
     );
   }
